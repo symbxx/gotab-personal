c33e4f135b1ae2ba9396441f02f734e3d131f6eb
dengxiwang <dengxiwang@vip.qq.com>
2025-07-21 19:13:56 +0800

    修复插件版全局搜索结果未加域名前缀的问题

---
+++ web/assets/useEffectOnce-CLWJY0ZK.js
@@ -0,0 +1,1 @@
+import{r as e}from"./requestIdleCallbackPolyfill-CVD_37F6.js";var l=function(l){e.useEffect(l,[])};export{l as u};
--- web/assets/useEffectOnce-CNE3l1oO.js
+++
@@ -1,1 +0,0 @@
-import{r as e}from"./requestIdleCallbackPolyfill-BYpT1Coe.js";var l=function(l){e.useEffect(l,[])};export{l as u};
