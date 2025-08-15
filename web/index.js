a51d4e0d592b865ca2055cac5064e6d1b5d0da80
dengxiwang <dengxiwang@vip.qq.com>
2025-08-15 18:17:40 +0800

    修复卡片的props传递因深比较导致需刷新才生效的问题

--- web/index.js
+++ web/index.js
@@ -1,1 +1,1 @@
-import{d as s,s as a,S as e,a as o,R as n,j as t,M as m}from"./assets/main-D7pK9nFI.js";s.locale("zh-cn"),a(!1),e.mount(new o),n.createRoot(document.body).render(t.jsx(m,{}));
+import{d as s,s as a,S as e,a as o,R as n,j as t,M as m}from"./assets/main-CljPVD0S.js";s.locale("zh-cn"),a(!1),e.mount(new o),n.createRoot(document.body).render(t.jsx(m,{}));
