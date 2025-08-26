706aded67f0bbc3f75dcc95af9d0ff680c7e7d4d
dengxiwang <dengxiwang@vip.qq.com>
2025-08-26 09:53:29 +0800

    优化暗夜模式的背景颜色设置，修复高斯模糊拖拽合并文件夹失效的问题

--- web/newtab.js
+++ web/newtab.js
@@ -1,1 +1,1 @@
-import{d as s,s as a,S as o,a as t,R as e,j as n,b as c,M as d}from"./assets/main-BpZjJVsn.js";s.locale("zh-cn"),a(!1),o.mount(new t),e.createRoot(document.body).render(window.location.protocol.includes("http")?n.jsx(c,{}):n.jsx(d,{type:"ext"}));
+import{d as s,s as a,S as o,a as t,R as e,j as n,b as c,M as d}from"./assets/main-B2vHLlcE.js";s.locale("zh-cn"),a(!1),o.mount(new t),e.createRoot(document.body).render(window.location.protocol.includes("http")?n.jsx(c,{}):n.jsx(d,{type:"ext"}));
